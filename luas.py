0ff125e5829fbd963408997bb4fc9217417c0ec2
java54 <hamandaraihan14@gmail.com>
2022-03-18 01:56:24 +0700

    intitial luas

---
+++ luas.py
@@ -0,0 +1,10 @@
+def hitung_luas_lingkaran(r):
+    luas = 3.14 * r**2
+    return luas
+
+def hitung_luas_persegi(s):
+    luas = s * s
+    return luas
+
+print ('luas lingkaran dengan diameter 7 adalah ', hitung_luas_lingkaran(7))
+print ('luas persegi dengan panjang sisi 4 adalah ', hitung_luas_persegi(4))
